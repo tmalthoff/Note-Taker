bed232f237dc5ac786cc25482c51d916038da1a3
Thomas Althoff <matthewalthoff1@gmail.com>
2020-12-06 14:00:39 -0600

    Make port configurable via environment variable

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const db = require('./db')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
